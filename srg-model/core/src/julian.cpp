c973dad1578d8fbab5a238f64537e1b0ffdf37a0
sbsin-lab <agadzhiev71@gmail.com>
2022-01-19 23:52:43 +0300

    Julian date proceeded

--- srg-model/core/src/julian.cpp
+++ srg-model/core/src/julian.cpp
@@ -5,38 +5,83 @@
 double cal2jd(int day, int month, int year, int hour, int min,
     int sec, int msec)
 {
-    return 0;
+    int a = (14 - month) / 12;
+    int y = year + 4800 - a;
+    int m = month + 12 * a - 3;
+    int jdn = day + (153 * m + 2) / 5 + 365 * y + y / 4 - y / 100 + y / 400 - 32045; // Номер юлианского дня (без дробной части)
+    double jd = static_cast <double>(jdn) + (static_cast <double>(hour) - 12) / 24 + static_cast <double>(min) / 1440 + static_cast <double>(sec) / 86400 + static_cast <double> (msec) / 86400000; // Юлианская дата (с дробной частью)
+    return jd;
 }
 
 // Юлианская дата -> Календарная дата.
 void jd2cal(double jd, int& day, int& month, int& year, int& hour,
     int& min, int& sec, int& msec)
 {
-    day = month = year = hour = min = sec = msec = 0;
+    int jdn = static_cast<int>(jd);
+    int a = jdn + 32044;
+    int b = (4 * a + 3) / 146097;
+    int c = a - (146097 * b) / 4;
+    int d = (4 * c + 3) / 1461;
+    int e = c - (1461 * d) / 4;
+    int m = (5 * e + 2) / 153;
+
+    day = e - (153 * m + 2) / 5 + 1;
+    month = m + 3 - 12 * (m / 10);
+    year = 100 * b + d - 4800 + m / 10;
+
+    double fraction = jd - jdn;
+    int msecs_total = 86400000 * fraction + 43200000; // Число миллисекунд, прошедшее с начала суток указанного дня
+    int pr = (msecs_total % 3600000);
+
+    hour = (msecs_total / 3600000); // Часы
+    min = pr / 60000; // Минуты
+    sec = (pr % 60000) / 1000; //Секунды
+    msec = (pr % 60000) % 1000;
 }
 
 // Юлианская дата -> Модифицированная юлианская дата.
 double jd2mjd(double jd)
 {
-    return 0;
+    return (jd - 2400000.5);
 }
 
 // Модифицированная юлианская дата -> Юлианская дата.
 double mjd2jd(double mjd)
 {
-    return 0;
+    return (mjd + 2400000.5);
 }
 
 // Календарная дата -> Модифицированная юлианская дата.
 double cal2mjd(int day, int month, int year, int hour, int min, int sec,
     int msec)
 {
-    return 0;
+    return (jd2mjd(cal2jd(day, month, year, hour, min, sec, msec)));
 }
 
 // Модифицированная юлианская дата -> Календарная дата.
 void mjd2cal(double mjd, int& day, int& month, int& year, int& hour, int& min,
     int& sec, int& msec)
 {
-    day = month = year = hour = min = sec = msec = 0;
+    double jd = mjd2jd(mjd);
+
+    int jdn = static_cast<int>(jd);
+    int a = jdn + 32044;
+    int b = (4 * a + 3) / 146097;
+    int c = a - (146097 * b) / 4;
+    int d = (4 * c + 3) / 1461;
+    int e = c - (1461 * d) / 4;
+    int m = (5 * e + 2) / 153;
+
+    day = e - (153 * m + 2) / 5 + 1;
+    month = m + 3 - 12 * (m / 10);
+    year = 100 * b + d - 4800 + m / 10;
+
+    double fraction = jd - jdn;
+    int msecs_total = 86400000 * fraction + 43200000; // Число миллисекунд, прошедшее с начала суток указанного дня
+    int pr = (msecs_total % 3600000);
+
+    hour = (msecs_total / 3600000); // Часы
+    min = pr / 60000; // Минуты
+    sec = (pr % 60000) / 1000; //Секунды
+    msec = (pr % 60000) % 1000;
 }
--- srg-model/sandbox/main.cpp
+++ srg-model/sandbox/main.cpp
@@ -3,5 +3,22 @@
 int main()
 {
     std::cout << "SANDBOX" << std::endl;
+    double a = 0.344965;
+    int msecs = 86400000 * a + 43200000; // Число миллисекунд, прошедшее с начала суток указанного дня
+    int b = (msecs % 3600000);
+    int hours = (msecs / 3600000); // Часы
+    int minutes = b / 60000; // Минуты
+    int seconds = (b % 60000) / 1000; //Секунды
+    int mseconds = (b % 60000) % 1000;
+    
+    int as = 5;
+    double ba = 5.5;
+    std::cout << as - ba;
+    std::cout << "\n" << hours << "\n" << minutes << "\n" << seconds << "\n" << mseconds;
     return 0;
 }
+
+
+
+// 1275 минут
+// 1275 div 60
