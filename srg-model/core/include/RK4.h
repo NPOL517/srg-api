3497bf42420824a2ae303a6307ec220609c9d304
sbsin-lab <agadzhiev71@gmail.com>
2022-02-21 23:03:05 +0300

    Реализация метода Рунге-Кутта старших порядков

--- srg-model/core/include/RK4.h
+++ srg-model/core/include/RK4.h
@@ -8,9 +8,10 @@ void rk_sin(double& x_0, double& y_0, double h);
 // Функция правых частей
 std::array<double, 6> function_of_right_values(std::array<double, 6>& arr);
 
-// Рунге-Кутта 4 порядка для задачи двух тел
+// Реализация метода Рунге-Кутта с помощью Таблицы Бутчера
 void dopri8_fixed(double& time, std::array<double, 6>& arr_baz, double h);
 
+// Обертка для метода Рунге-Кутта с заданным интервальным значением
 void dopri8(double &time, std::array<double, 6>& state, double interval);
 
 
--- srg-model/sandbox/main.cpp
+++ srg-model/sandbox/main.cpp
@@ -5,29 +5,10 @@
 #include <math.h>
 
 
-// Пример вызова dopri8_fixed
-int usage_dopri8_fixed()
+// Пример вызова dopri8
+int usage_dopri8(double &time, std::array<double, 6> arr, double h)
 {
-    std::array<double, 6> arr = { -3161.946517, 2801.776225, -5322.279908, -4.795367, -5.966911, -0.292216 };
-    double time = 0;
-    std::array<double, 6> arr_modified = dopri8_fixed(time, arr, 1);
-    FILE* n = fopen("dopri8_fixed.txt", "w");
-    if (n == NULL) {
-        std::cout << "Failed file opening";
-        return -1;
-    }
-    else {
-        for (int h = 1; h < 1.5 * 60 * 60; h++)
-        {
-            fprintf(n, "%f %f %f\n", arr_modified[0], arr_modified[1], arr_modified[2]);
-            arr_modified = dopri8_fixed(time, arr_modified, 1);
-        }
-    }
-    for (auto v : arr)
-        std::cout << v << std::endl;
-    std::cout << "\n\n\n";
-    for (auto v : arr_modified)
-        std::cout << v << std::endl;
+    dopri8(time, arr, h);
     return 0;
 }
 
@@ -35,6 +16,13 @@ int usage_dopri8_fixed()
 
 int main()
 {
-    usage_dopri8_fixed();
+    std::array<double, 6> arr = { -3161.946517, 2801.776225, -5322.279908, -4.795367, -5.966911, -0.292216 };
+    double time = 0;
+
+    usage_dopri8(time, arr, 86400);
+    
+    for (int i = 0; i < 6; i++)
+        std::cout << arr[i] << " ";
+    std::cout << "Time: " << time;
     return 0;
 }
